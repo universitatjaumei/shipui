c8881ebba87e9e2a1c7fb03d7be444916a724499
David Rubert <david.rubert@gmail.com>
2015-09-09 08:23:39 +0200

    Fusionamos el servidor de gestión de configuraciones con el de gestión de deployments

---
+++ run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+pushd `dirname $0` > /dev/null
+
+docker rm -f ship/shipui
+
+docker build -t ship/shipui dockerized-shipui
+
+cp -a ../ship/ship  $(pwd)
+
+docker run -d -p 5000:5000 -v /opt/devel/workspaces/uji/shipui:/src --name shipui ship/shipui
+
+popd > /dev/null
