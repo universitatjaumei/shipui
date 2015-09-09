23068bbaeb6f9f9265e61d91c6c56159e81a18ec
David Rubert <david.rubert@gmail.com>
2015-09-09 09:13:28 +0200

    Separamos las rutas de gestión de configuración en un archivo independiente

---
+++ routes/conf.py
@@ -0,0 +1,50 @@
+import flask
+from config import AppsConfiguration
+
+conf_app = flask.Blueprint('conf_app', __name__, template_folder='../templates')
+
+@conf_app.route("/etcd")
+def page_apps():
+    apps_conf = AppsConfiguration()
+    conf = apps_conf.get()
+    return flask.render_template('apps.html', conf=conf)
+
+
+@conf_app.route("/conf", methods=['GET'])
+def all_apps():
+    apps_conf = AppsConfiguration()
+    return flask.jsonify(apps_conf.get())
+
+
+@conf_app.route("/conf/<app>", methods=['GET'])
+def app_config(app):
+    apps_conf = AppsConfiguration()
+    return apps_conf.serialize_app(app)
+
+
+@conf_app.route("/conf/<app>", methods=['PUT'])
+def app_config_save(app):
+    props = flask.request.form.get("props")
+
+    apps_conf = AppsConfiguration()
+    apps_conf.save_app(app, props)
+
+    return props
+
+
+@conf_app.route("/conf/<app>", methods=['POST'])
+def new_app(app):
+    host = flask.request.form.get("host")
+
+    apps_conf = AppsConfiguration()
+    apps_conf.new(app, host)
+
+    return 'ok'
+
+
+@conf_app.route("/conf/<app>", methods=['DELETE'])
+def delete_app(app):
+    apps_conf = AppsConfiguration()
+    apps_conf.delete(app)
+
+    return 'ok'
--- server.py
+++ server.py
@@ -1,60 +1,15 @@
 import flask
-from config import AppsConfiguration
 from routes.deploy import deploy_app
+from routes.conf import conf_app
+
 
 app = flask.Flask(__name__)
 app.register_blueprint(deploy_app)
+app.register_blueprint(conf_app)
 
 @app.route("/", methods=["GET"])
 def index():
     return flask.render_template("index.html")
-
-@app.route("/etcd")
-def page_apps():
-    apps_conf = AppsConfiguration()
-    conf = apps_conf.get()
-    return flask.render_template('apps.html', conf=conf)
-
-
-@app.route("/conf", methods=['GET'])
-def all_apps():
-    apps_conf = AppsConfiguration()
-    return flask.jsonify(apps_conf.get())
-
-
-@app.route("/conf/<app>", methods=['GET'])
-def app_config(app):
-    apps_conf = AppsConfiguration()
-    return apps_conf.serialize_app(app)
-
-
-@app.route("/conf/<app>", methods=['PUT'])
-def app_config_save(app):
-    props = flask.request.form.get("props")
-
-    apps_conf = AppsConfiguration()
-    apps_conf.save_app(app, props)
-
-    return props
-
-
-@app.route("/conf/<app>", methods=['POST'])
-def new_app(app):
-    host = flask.request.form.get("host")
-
-    apps_conf = AppsConfiguration()
-    apps_conf.new(app, host)
-
-    return 'ok'
-
-
-@app.route("/conf/<app>", methods=['DELETE'])
-def delete_app(app):
-    apps_conf = AppsConfiguration()
-    apps_conf.delete(app)
-
-    return 'ok'
-
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
