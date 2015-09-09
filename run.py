c4f3503d9578625429dc444f1c3fc6a4ab1dd596
David Rubert <david.rubert@gmail.com>
2015-09-09 08:23:39 +0200

    Fusionamos el servidor de gestión de configuraciones con el de gestión de deployments

--- run.py
+++ run.py
@@ -1,63 +1,21 @@
-import flask
-from config import AppsConfiguration
 from ship.logger import ShipLogger
 from ship.project import ProjectBuilder
 from ship.validator import *
 from ship.errors import SVNException, ProjectIdNotFoundException
+from flask import Flask, request, render_template
 import urllib2
 import yaml
 import simplejson
 import traceback
 
-app = flask.Flask(__name__)
+app = Flask(__name__)
 
-
-@app.route("/")
-def page_apps():
-    apps_conf = AppsConfiguration()
-    conf = apps_conf.get()
-    return flask.render_template('apps.html', conf=conf)
-
-
-@app.route("/conf", methods=['GET'])
-def all_apps():
-    apps_conf = AppsConfiguration()
-    return flask.jsonify(apps_conf.get())
-
-
-@app.route("/conf/<app>", methods=['GET'])
-def app_config(app):
-    apps_conf = AppsConfiguration()
-    return apps_conf.serialize_app(app)
-
-
-@app.route("/conf/<app>", methods=['PUT'])
-def app_config_save(app):
-    props = flask.request.form.get("props")
-
-    apps_conf = AppsConfiguration()
-    apps_conf.save_app(app, props)
-
-    return props
-
-
-@app.route("/conf/<app>", methods=['POST'])
-def new_app(app):
-    host = flask.request.form.get("host")
-
-    apps_conf = AppsConfiguration()
-    apps_conf.new(app, host)
-
-    return 'ok'
-
-
-@app.route("/conf/<app>", methods=['DELETE'])
-def delete_app(app):
-    apps_conf = AppsConfiguration()
-    apps_conf.delete(app)
-
-    return 'ok'
-
+ENVIRONMENT = "production"
+HOME = "/tmp/target"
+URL = "svn://ship-svn/repos/SAMPLE"
+PROJECT_NAME = "sample"
+KEY = "smp"
+VERSION = "trunk"
 
 def get_app_acronyms():
     req = urllib2.Request("http://infra01.uji.es:4001/v2/keys/ujiapps/apps/all.yaml")
@@ -68,12 +26,6 @@ def get_app_acronyms():
     apps = [ app.upper() for app in data.keys() ]
     apps.sort()
     return apps
-
-@app.route("/deploy", methods=["GET"])
-def index():
-    apps = get_app_acronyms()
-    return flask.render_template("deploy.html", apps=apps)
-
 
 @app.route("/deploy", methods=["POST"])
 def deploy():
@@ -116,6 +68,11 @@ def deploy():
 
     return  "ole :)"
 
+
+@app.route("/post", methods=["GET"])
+def index():
+    apps = get_app_acronyms()
+    return render_template("deploy.html", apps=apps)
 
 
 if __name__ == "__main__":
