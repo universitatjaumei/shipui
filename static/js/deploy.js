b38c84d0a6bfd764fe91e7cd362713e56f4a71e7
David Rubert <david.rubert@gmail.com>
2015-11-09 13:11:49 +0100

    Añadimos una capa básica de CSS a la aplicación

---
+++ static/js/deploy.js
@@ -0,0 +1,57 @@
+function showMessageDeploying() {
+  $('.deploying-message').show();
+  $('.deploying-message').removeClass('success');
+  $('.deploying-message .spinner').show();
+  $('.deploying-message .sk-folding-cube').hide();
+  $('.deploying-message .title').html('Deploying...');
+
+  $('.log').css('opacity', '1.0');
+  $('.log').css('border', '1px solid #AAA');
+}
+
+function showMessageDeployFinished() {
+  $('.deploying-message').addClass('success');
+  $('.deploying-message .spinner').hide();
+  $('.deploying-message .sk-folding-cube').show();
+  $('.deploying-message .title').html('Done!');
+}
+
+$(document).ready(function() {
+
+  $('form button').click(function() {
+    //socket.emit('deploy start event', {data: $("select option:selected").text()});
+    showMessageDeploying();
+
+    $.ajax({
+      type: 'POST',
+      url: $('form.deploy-form').attr('action'),
+      data: JSON.stringify({ app: $('select option:selected').text() }, null, '\t'),
+      contentType: 'application/json;charset=UTF-8',
+      success: function(data, status) {
+        showMessageDeployFinished();
+      },
+    });
+
+    return false;
+  });
+
+  return;
+
+  var socket = io.connect('http://' + document.domain + ':' + location.port + '/deploy');
+  socket.on('disconnect', function() {
+    console.log('disonnected');
+  });
+
+  socket.on('deploy-log', function(response) {
+    $('pre.log').html(ansispan(response.data));
+    socket.emit('received', 'ok');
+  });
+
+  socket.on('deploy finished event', function(response) {
+    var current = $('pre.log').html();
+    $('pre.log').html(current + '\nFinished.\n');
+    running = false;
+    $('.double-bounce1, .double-bounce2').hide();
+  });
+
+});
