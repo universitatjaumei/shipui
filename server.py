a2f30ea6960f7261b32a7256f2aaf097d2bb27e3
David Rubert <david.rubert@gmail.com>
2015-11-09 13:10:15 +0100

    Mejor gestión de logs

--- server.py
+++ server.py
@@ -1,13 +1,19 @@
 import flask
+import threading
 from routes.deploy import deploy_app
 from routes.configuration import conf_app
 from routes.properties import properties_app
 import logging
 import sys
+import io
+import time
 import flask_lsm_auth
+import StringIO
+import time
+from ship.logger import ShipLogger
 
 app = flask.Flask("shipui")
-
+app.config['SECRET_KEY'] = 'secretor!!#!'
 
 app.register_blueprint(deploy_app)
 app.register_blueprint(conf_app)
@@ -17,6 +23,7 @@ app.register_blueprint(properties_app)
 def index():
     return flask.redirect("/deploy")
 
+
 @app.route("/logout", methods=["GET"])
 def logout():
     lsm = flask_lsm_auth.LSM()
@@ -25,26 +32,32 @@ def logout():
     return lsm.compose_response()
 
 
-def setup_log():
-    logger = logging.getLogger("werkzeug")
-    handler = logging.StreamHandler()
-    handler.setLevel(logging.getLevelName("INFO"))
-    handler.setFormatter(logging.Formatter("[shipui] %(levelname)s %(message)s"))
-    logger.setLevel(logging.INFO)
-    logger.addHandler(handler)
-
 @app.after_request
 def after_request(res):
-
     lsm = flask_lsm_auth.LSM()
-    app.logger.info("Requesting %s...", flask.request.url)
-
     if not lsm.get_login():
         lsm.login()
 
     return lsm.compose_response(res)
 
+
+@app.before_first_request
+def setup_logging():
+    logger = logging.getLogger('werkzeug')
+    logger.setLevel(logging.INFO)
+
+    for handler in logger.handlers:
+        logger.removeHandler(handler)
+
+    handler = logging.StreamHandler()
+    handler.setFormatter(logging.Formatter("[shipui] %(levelname)s %(message)s"))
+    handler.setLevel(logging.INFO)
+    logger.addHandler(handler)
+
 if __name__ == "__main__":
 
-    setup_log()
-    app.run(host="0.0.0.0", debug=True)
+    try:
+        app.run(host="0.0.0.0", debug=True)
+    except KeyboardInterrupt:
+        t.join()
+        print "bye"
